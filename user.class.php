c23ac3e75672743a0341cf33580df7db3cecdc13
Alberto <spiff@fear>
2012-07-28 21:06:23 +0200

    Some fixes... not DB integration yet

--- user.class.php
+++ user.class.php
@@ -1,79 +1,82 @@
 <?php 
-	class User{
+class User{
 
-		private $id_user;
-		private $nickname;
-		private $email;
-		private $points;
-	
+	private $id_user;
+	private $nickname;
+	private $email;
+	private $points;
+
         function __construct($id_user = null, $nickname = null, $email = null, $points = null){
         	if($id_user == null){
-        		$id_user = -2;
+        		$this->id_user = -2;
         		return;
         	}
-        	//continuar constructor
+        	$this->setId_user($id_user);
+		$this->setNickname($nickname);
+		$this->setEmail($email);
+		$this->setPoints($points);
         }
 
-        function changePoints($change){
-        	$points += $change;
+        function addPoints($change){
+        	$this->points += $change;
         }
 
         function exists(){
         	//comprueba si existe este usuario en la db
         }
         
-		//******* GETS *********************        
-	    function getId_user(){
-	    	return $id_user;
-	    }        
+	//******* GETS *********************        
+	function getId_user(){
+		return $this->id_user;
+	}        
 
-	    function getNickname(){
-	    	return $nickname;
-	    }        
+	function getNickname(){
+		return $this->nickname;
+	}        
 
-	    function getEmail(){
-	    	return $email;
-	    }        
+	function getEmail(){
+		return $this->email;
+	}        
 
-	    function getPoints(){
-	    	return $points;
-	    } 
-	    //**********************************       
+	function getPoints(){
+		return $this->points;
+	} 
+	//**********************************       
 
-	    function isLogged(){
-	    	return $id_user > 0;
-	    }        
+	function isLogged(){
+		return $this->id_user > 0;
+	}        
 
-	    function login($email,$pass){
-	    	//...
-	    } 
-	           
-	    function logout(){
-	    	$id_user = -3;
-	    }
-	            
-		//******* SETS *********************
-	    function setId_user($id_user){
-	    	$this->id_user = $id_user;
-	    }        
-
-	    function setNickname($nickname){
-	    	$this->nickname = $nickname;
-	    }        
-
-	    function setEmail($email){
-	    	$this->email = $email;
-	    }        
-
-	    function setPoints($points){
-	    	$this->points = $points;
-	    }	    
-	    //**********************************    
-
-	    function update(){
-	    	//guarda los cambios en la db
-	    }   
+	function login($email, $pass){
+		//...
+	} 
+	   
+	function logout(){
+		$this->id_user = -3;
 	}
+	    
+	//******* SETS *********************
+	function setId_user($id_user){
+		$this->id_user = $id_user;
+	}        
+
+	function setNickname($nickname){
+		$this->nickname = $nickname;
+	}        
+
+	function setEmail($email){
+		$this->email = $email;
+	}        
+
+	function setPoints($points){
+		$this->points = $points;
+	}	    
+	//**********************************    
+
+	function update(){
+		//guarda los cambios en la db
+	}   
+}
 
 	/*
 		INVENTADO
